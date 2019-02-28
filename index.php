f5ba4df1ae18d7a1bddeb82a42bf8574ddc6b8a1
Felipe Sales <felipesales007@hotmail.com>
2019-02-28 18:23:47 -0300

    inclusão da dacumentação do aos (animações)

--- index.php
+++ index.php
@@ -4,6 +4,9 @@
     // header("location: https://www.felipesales.com.br/sites/views/site");
 
     // link em homologação
-    // documentação: https://demos.creative-tim.com/material-kit-pro/docs/2.0/components/carousel.html
     header("location: app/site/home/index");
+
+    // documentações
+    // documentação: https://demos.creative-tim.com/material-kit-pro/docs/2.0/components/carousel.html
+    // https://michalsnik.github.io/aos/
 ?>
